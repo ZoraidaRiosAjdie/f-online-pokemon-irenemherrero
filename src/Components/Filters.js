a555f3ba38ccc13db64a32204f9f5a2cbde9b615
irenemherrero <irenemherrero@gmail.com>
2018-10-06 10:15:40 +0200

    create input

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 class Filters extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <p>Holi</p>
-         );
+    state = {}
+    render() {
+        return (
+            <div>
+                <label for="pokeSearch">Introduce el nombre de tu pokemon: </label>
+                <input id="pokeSearch" type="text" name="pokemonName" />
+            </div>
+        );
     }
 }
- 
+
 export default Filters;
