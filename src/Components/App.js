6efc3e5c57fee3545a625faf6c10b260527195e9
irenemherrero <irenemherrero@gmail.com>
2018-10-06 10:13:01 +0200

    saved pokemons in state

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,38 +8,37 @@ let arrayPokemons = [];
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      pokemons: []
+     }
     this.fetchData=this.fetchData.bind(this);
 }
 
 componentDidMount(){
-  const URL = 'http://pokeapi.salestock.net/api/v2/pokemon/'
-  const numberPokemonToSearch = 25;
+  const URL = 'https://pokeapi.co/api/v2/pokemon/'
+  const numberPokemonToSearch = 4;
   for(let i = 1; i < numberPokemonToSearch+1; i++){
-    this.fetchData(`${URL}${i}`);
+    this.fetchData(`${URL}${i}/`);
     }
   }
 
   fetchData(url){
     fetch(url)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(json){
+    .then(response => response.json())
+    .then(json => {
       console.log(json);
-      arrayPokemons = [
-        ...arrayPokemons,
+      arrayPokemons[json.id-1] = 
         {
-          id: json.id,
-          name: json.name,
-          photo: json.sprites.front_default,
-          types: "",
-        }
-      ];
-      console.log(arrayPokemons);
-      // for(let j = 0; j < numberOfPokemon; j++)
-
-      // })
+              id: json.id,
+              name: json.name,
+              photo: json.sprites.front_default,
+              types: "",
+            };
+      json.types.map(type => arrayPokemons[json.id-1].types = [...arrayPokemons[json.id-1].types, type.type.name,]
+      );
+      this.setState({
+        pokemons: arrayPokemons,
+      }, console.log(this.state.pokemons));
   })
 }
 
