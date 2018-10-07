43ec7f896023268fde91b18fb14a2341716fe1a8
irenemherrero <irenemherrero@gmail.com>
2018-10-07 10:35:39 +0200

    add local storage

--- src/Components/App.js
+++ src/Components/App.js
@@ -12,23 +12,32 @@ class App extends Component {
     this.state = {
       pokemonList: [],
       filteredList: [],
-      searchValue: null
+      searchValue: ""
     };
     this.fetchData = this.fetchData.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
     this.selectListToPrint = this.selectListToPrint.bind(this);
+    this.saveDataInState = this.saveDataInState.bind(this);
+    this.saveDataInLocalStorage = this.saveDataInLocalStorage.bind(this);
   }
 
-  //1. Preparar llamada a la API
+  //1. Rescatar datos de LS (si hay) o preparar llamada a la API
 
   componentDidMount() {
-    const URL = "https://pokeapi.co/api/v2/pokemon/";
-    for (let i = 0; i < numberPokemonToSearch; i++) {
-      this.fetchData(`${URL}${i + 1}/`);
-    }
-  }
+    const listFromLocalStorage = JSON.parse(localStorage.getItem("pokemon-list"));
+    if (listFromLocalStorage) {
+      this.setState({
+        pokemonList: listFromLocalStorage
+      });
+    } else {
+      const URL = "https://pokeapi.co/api/v2/pokemon/";
+      for (let i = 0; i < numberPokemonToSearch; i++) {
+        this.fetchData(`${URL}${i + 1}/`);
+      };
+    };
+  };
 
-  // 2. Llamada a la API y meter datos de la API en el estado.
+  // 2. Llamada a la API y construcción de objeto con datos de pokemon.
 
   fetchData(url) {
     fetch(url)
@@ -47,13 +56,28 @@ class App extends Component {
               type.type.name
             ])
         );
-        this.setState({
-          pokemonList: arrayPokemons
-        });
+        this.saveDataInState();
       });
-  }
+  };
 
-  //3. Controlar el evento del input: cuando escribes, se filtra la lista de pokemon inicial y los filtrados se meten en el array filteredList del estado.
+  //3. Meter datos de la API en el estado
+
+  saveDataInState() {
+    this.setState(
+      {
+        pokemonList: arrayPokemons
+      },
+      this.saveDataInLocalStorage()
+    );
+  };
+
+  //4. Guardar datos en el Local Storage
+
+  saveDataInLocalStorage() {
+    localStorage.setItem("pokemon-list", JSON.stringify(arrayPokemons));
+  };
+
+  //5. Controlar el evento del input: cuando escribes, se filtra la lista de pokemon inicial y los filtrados se meten en el array filteredList del estado.
 
   handleSearch(e) {
     const search = e.target.value.toLowerCase();
@@ -63,29 +87,24 @@ class App extends Component {
         return pokemon.name.includes(search);
       })
     });
-  }
+  };
 
-  //4. Seleccinar la lista de pokemon que renderizar: la inicial o la filtrada
+  //6. Seleccinar la lista de pokemon que renderizar: la inicial o la filtrada
 
   selectListToPrint() {
-    const { 
-      filteredList, 
-      pokemonList, 
-      searchValue } = this.state;
+    const { filteredList, pokemonList, searchValue } = this.state;
     return !searchValue ? pokemonList : filteredList;
-  }
+  };
 
   render() {
-    return this.state.pokemonList.length === numberPokemonToSearch ? (
-      <Main
+    return this.state.pokemonList.length === numberPokemonToSearch 
+    ? <Main
         handleSearch={this.handleSearch}
         pokemonList={this.selectListToPrint}
         searchValue={this.state.searchValue}
       />
-    ) : (
-      <LoadingPage />
-    );
-  }
-}
+    : <LoadingPage />
+  };
+};
 
 export default App;
