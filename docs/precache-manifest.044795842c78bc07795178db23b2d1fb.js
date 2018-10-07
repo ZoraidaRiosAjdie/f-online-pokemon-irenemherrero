7bb878dd73b051927438ef80587cf231030e3d5b
irenemherrero <irenemherrero@gmail.com>
2018-10-07 10:05:45 +0200

    preparing deploy

---
+++ docs/precache-manifest.044795842c78bc07795178db23b2d1fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3aa872f8d07204e811dde23ab5c8f40f",
+    "url": "./static/media/pikachu_loading.3aa872f8.gif"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c6e0ded085cb552ed1bb",
+    "url": "./static/js/main.c6e0ded0.chunk.js"
+  },
+  {
+    "revision": "51dc899d278e96705dc4",
+    "url": "./static/js/1.51dc899d.chunk.js"
+  },
+  {
+    "revision": "c6e0ded085cb552ed1bb",
+    "url": "./static/css/main.873c001c.chunk.css"
+  },
+  {
+    "revision": "c421c8e1c48f518c512d9d811764b2c9",
+    "url": "./index.html"
+  }
+];
